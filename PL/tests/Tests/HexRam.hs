20cfda30a8cdc1bb0c26d7a0a7b83968f1e85074
TheStoneWolf <simonj12@hotmail.se>
2026-06-03 21:31:28 +0200

    generate VCD files that can be used in Surfer the Waveform Viewer

--- PL/tests/Tests/HexRam.hs
+++ PL/tests/Tests/HexRam.hs
@@ -2,6 +2,8 @@ module Tests.HexRam where
 
 import Clash.Hedgehog.Sized.Unsigned
 import qualified Clash.Prelude as C
+import qualified Clash.Shockwaves as SW
+import Data.Text.IO (writeFile)
 import qualified Example.Hex as Hex
 import qualified Hedgehog as H
 import qualified Hedgehog.Gen as Gen
@@ -10,7 +12,9 @@ import HexRam
 import Test.Tasty
 import Test.Tasty.Hedgehog
 import Test.Tasty.TH
-import Prelude
+import Prelude hiding (writeFile)
+
+C.createDomain C.vSystem {C.vName = "TestDom", C.vPeriod = 1000}
 
 type AddrWidth = 4
 
@@ -36,8 +40,19 @@ prop_read_ram = H.property $ do
     H.forAll (Gen.list (Range.singleton simDuration) genHexCoord)
 
   -- The output register of the BRAM is undefined on startup, therefore drop it
-  let simOut = drop 1 $ fromIntegral <$> C.sampleN (simDuration + 1) (hexRam @C.System @AddrWidth @DataD (C.fromList inp) (pure Nothing)) :: [Int]
+  let inputAddr = C.fromList inp
+      -- Domain, clk, rst and ena must be defined due to VCD dump requiring it
+      simOutSignal = C.withClockResetEnable @TestDom C.clockGen C.resetGen C.enableGen $ hexRam @TestDom @AddrWidth @DataD inputAddr (pure Nothing)
+      simOut = drop 1 $ fromIntegral <$> C.sampleN (simDuration + 1) simOutSignal :: [Int]
       expected = replicate simDuration 3
+
+  vcddata <- H.evalIO $ SW.dumpVCD (0, simDuration + 1) (SW.traceSignal "inputAddr" simOutSignal) []
+  case vcddata of
+    Left msg ->
+      error msg
+    Right (vcd, meta) -> do
+      H.evalIO $ writeFile "waveforms/waveform.vcd" vcd
+      H.evalIO $ SW.writeFileJSON "waveforms/waveform.json" meta
 
   -- Check that the simulated output matches the expected output
   H.annotate $ "expected: " <> show expected
